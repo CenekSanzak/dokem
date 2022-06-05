e5b03de3ad99a0da88779a47b87321245700c9d3
bikem <bengisuozaydin@gmail.com>
2022-06-05 22:37:49 +0300

    parametrized everything

--- generate_all.py
+++ generate_all.py
@@ -15,7 +15,19 @@ LAYERS = {
 }
 
 
-def generate(sectors, _current_sector: Tuple[int, int], _half_sector_count=5):
+def generate(sectors,
+             _current_sector: Tuple[int, int],
+             sector_size=1000.0,
+             sector_margin_proportion=0.1,
+             sector_axis_count=10,
+             start_seed="world generation",
+             planet_generation_area_threshold=5000.0,
+             moon_generation_chance=1.1 / 3.0,
+             max_moon_count=5,
+             planet_base_size=96,
+             asteroid_generation_chance=3.0 / 4.0,
+             max_asteroid_count=10):
+    _half_sector_count = int(sector_axis_count / 2.0)
     start_time = time.time()
     index = -1
     for layer in LAYERS:
@@ -32,15 +44,33 @@ def generate(sectors, _current_sector: Tuple[int, int], _half_sector_count=5):
                 if layer == "seeds":
                     if sector not in sectors:
                         sectors.update({str(sector): {}})
-                    _generate_seeds_at(sector=sector, sectors=sectors)
+                    _generate_seeds_at(sector=sector,
+                                       sectors=sectors,
+                                       start_seed=start_seed,
+                                       sector_size=sector_size,
+                                       sector_margin_proportion=sector_margin_proportion)
                 if layer == "planet":
-                    _generate_planets_at(sector=sector, sectors=sectors)
+                    _generate_planets_at(sector=sector,
+                                         sectors=sectors,
+                                         planet_generation_area_threshold=planet_generation_area_threshold)
                 if layer == "moons":
-                    _generate_moons_at(sector=sector, sectors=sectors)
+                    _generate_moons_at(sector=sector,
+                                       sectors=sectors,
+                                       start_seed=start_seed,
+                                       moon_generation_chance=moon_generation_chance,
+                                       max_moon_count=max_moon_count,
+                                       planet_base_size=planet_base_size)
                 if layer == "travel_lanes":
-                    _generate_travel_lanes_at(sector=sector, sectors=sectors)
+                    _generate_travel_lanes_at(sector=sector,
+                                              sectors=sectors)
                 if layer == "asteroids":
-                    _generate_asteroids_at(sector=sector, sectors=sectors)
+                    _generate_asteroids_at(sector=sector,
+                                           sectors=sectors,
+                                           start_seed=start_seed,
+                                           asteroid_generation_chance=asteroid_generation_chance,
+                                           max_asteroid_count=max_asteroid_count,
+                                           planet_base_size=planet_base_size)
+
     end_time = time.time()
     print((end_time - start_time))
 
